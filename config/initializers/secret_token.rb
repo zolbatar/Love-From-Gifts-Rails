68202294a8c574a63d947254eea78a333313a8f7
Daryl Dudey <daryljdudey@googlemail.com>
2020-02-19 13:00:11 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoveFromGifts::Application.config.secret_token = 'ba4ede46da7a390b19a0ebf50ca426bc3cc2274de985fe54902bcf2af708c70fe78d6d7ec28623abda1dfb2708a767bffb40e864d4ca4d36f485e3036dabfa5b'
